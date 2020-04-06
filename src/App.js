bb1a61d2cc181730ec9bbabb40a5fbf2f65f6da1
Riley Seaburg <riley@rds.marketing>
2020-04-06 19:36:15 -0400

    added search & alerts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,73 @@
-import React, { Fragment, Component } from 'react';
-import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem'
-import './App.css';
+import React, { Fragment, Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import PropTypes from "prop-types";
+
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
-  foo = () => 'Bars';
+  state = {
+    users: [],
+    loading: false,
+    alert: null,
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
+  };
+
+  ///////////////////////////////////
+  //        Depricated Code        //
+  ///////////////////////////////////
+
+  // async componentDidMount /*Lifecycle Method */() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHIB_CLIENT_ID}&clien_secret=${process.env.REACT_APP_GITHIB_CLIENT_SECRET}`
+  //   );
+  //   this.setState({ users: res.data, loading: false });
+  //   // console.log(res.data);
+  // }
+  // Search Users Callback Function Also Arrow & Async
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHIB_CLIENT_ID}&clien_secret=${process.env.REACT_APP_GITHIB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 1500);
+  };
+
   render() {
-    
+    const { users, loading } = this.state;
     return (
-      <div className="app" >
-      <Fragment>
-      <Navbar/>
-      <UserItem/>
-      </Fragment>
+      <div className="app">
+        <Navbar />
+        <div className="container">
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
+        </div>
       </div>
     );
   }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,24 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import propTypes from 'prop-types'; /*ES7 React Redux Snipit shortcode "IMpT" */
 
-class UserItem extends Component {
-    state = {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-    };
-    render() {
-        const { login, avatar_url, html_url } = this.state;
-        return (
-            <div className="card text-center" >
-                <img src={avatar_url} className="round-img" alt="github user" style={{width: '60px'}} />
-                <h3>{login}</h3>
-                <div>
-                <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-                </div>
-            </div>
-        );
-    }
+const UserItem = ({user: {login, avatar_url, html_url}}) => {
+    {
+  // state = {
+  //     id: 'id',
+  //     login: 'mojombo',
+  //     avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+  //     html_url: 'https://github.com/mojombo'
+  // }; /*Static State*/
 }
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        className="round-img"
+        alt="github user"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default UserItem
+UserItem.propTypes = {
+    user: propTypes.object.isRequired /*ES7 React Redux Snipit shortcode "pTOR" */
+};
+
+export default UserItem;
