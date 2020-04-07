7be337c3006a28b5da68c14b3ac51978a81c5db0
Riley Seaburg <riley@rds.marketing>
2020-04-06 20:52:09 -0400

    add production build

---
+++ build/precache-manifest.6266b90f792ab08cc0a0a000dd2c19ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7a78045613c9dd92e8ba83895b212f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed28078a40e11e19a3ed",
+    "url": "/static/css/main.e175b0cd.chunk.css"
+  },
+  {
+    "revision": "7d11854efce9ce4fbca3",
+    "url": "/static/js/2.5c84b6fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5c84b6fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed28078a40e11e19a3ed",
+    "url": "/static/js/main.ac4d878e.chunk.js"
+  },
+  {
+    "revision": "20dbaca900c4631cabb8",
+    "url": "/static/js/runtime-main.8b6e83ee.js"
+  }
+]);
