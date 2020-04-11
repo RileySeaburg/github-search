68479fc9e754205a55467c519c772dd77dd55f85
Riley Seaburg <riley@rds.marketing>
2020-04-11 07:52:25 -0400

    updated about and added enviornment variables

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -6,6 +6,12 @@ const About = () => {
       <h1>About This App</h1>
       <p>App to search github users</p>
       <p>v1.0.0</p>
+      <p className="">
+        Built By <a href="https://rileyseaburg.com"> Riley Seaburg</a>
+      </p>
+      <p>
+        Inspired by <a href="traverseymedia.com"> Brad Traversy</a>
+      </p>
     </div>
   );
 };
